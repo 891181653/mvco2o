dec8fe4920cc6e8809d25b059c5b68dc585fb80b
891181653 <891181653@qq.com>
2019-12-30 18:28:09 +0800

    11

---
+++ src/main/java/com/imooc/o2o/dao/AreaDao.java
@@ -0,0 +1,15 @@
+package com.imooc.o2o.dao;
+
+import java.util.List;
+
+
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Controller;
+
+import com.imooc.o2o.entity.Area;
+@Controller
+public interface AreaDao {
+	
+   List<Area> queryArea();
+}
